355d712231648273c9d346a0560fba61dd32f0bd
tyk <tyk@ldomotors.com>
2024-08-05 16:50:28 +0800

    添加测试用例

--- paser.py
+++ paser.py
@@ -1,12 +1,17 @@
-from paser_log.log_stats import LogKlipper, LogStats
+from paser_log.paser_log import LogKlipper, LogStats
 
 
 if __name__ == "__main__":
     file_path = "./logs/klippy.log"
 
-    file = open(file_path, "r")
-    log = file.read()
+    log = ""
+    with open(file_path, "r") as file:
+        log = file.read()
 
     cfg = LogKlipper(log)
     klipper_cfg = cfg.extract_newest_config()
     cfg.save_config(klipper_cfg)
+
+    stats = LogStats(file_path)
+    list_stats = stats.get_stats_dicts()
+    print(list_stats)
