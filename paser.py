83fbc7df9525bf207425f450e3e39b2663b48388
‘tyk’ <527273931@qq.com>
2024-08-06 23:50:24 +0800

    paser_log/paser_log.py 修复使用正则读取温度，有多个温度下无法区分， paser.py 使用趋近线表示数据（原数据太多，而且上下跳动，导致画图的y轴有问题）

--- paser.py
+++ paser.py
@@ -1,8 +1,10 @@
 from paser_log.paser_log import LogKlipper, LogStats
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    file_path = "./logs/klippy.log"
+    file_path = "./logs/klippy2.log"
 
     log = ""
     with open(file_path, "r") as file:
@@ -12,54 +14,154 @@ if __name__ == "__main__":
     klipper_cfg = cfg.extract_newest_config()
     cfg.save_to_file(klipper_cfg)
 
+    # 保存到文件
     stats = LogStats(file_path)
     list_dicts = stats.get_stats_dicts()
     stats.save_to_file(stats.get_stats_info())
 
+    # 解析out的文件
     stats_out_path = "./out/stats.cfg"
     stats = LogStats(stats_out_path)
     list_dicts = stats.get_stats_dicts()
 
-    try:
-        interval = 100  # sampling interval
-        i = 0
-        min_val = max_val = 0
-        list_retransmit = [0]
-        min_last_val = max_last_val = 0
-
-        for dicts in list_dicts:
-            min_last_val = min(
-                dicts["bytes_retransmit"]
-            )  # Here min_last_val and max_last_val are generally the same value
-            max_last_val = max(dicts["bytes_retransmit"])
-
-            if min_last_val < min_val:
-                min_val = min_last_val
-
-            if max_last_val > max_val:
-                max_val = max_last_val
-
-            i += 1
-            if interval == i:
-                i = 0
-                list_retransmit.append(max_val - min_val)
-                min_val = min_last_val  # Starting from the last result
-                max_val = max_last_val
-
-        if len(dicts) % interval != 0:
-            list_retransmit.append(max_val - min_val)
-        print(list_retransmit, len(list_retransmit))
-
-    except Exception as e:
-        print("异常：", e)
+    list_retransmit = stats.get_bytes_retransmit_incremental_list(100, list_dicts)
 
     # 画图
-    plt.plot(list_retransmit)
+    x_odd1 = range(len(list_retransmit))
 
-    # 添加标题和标签
-    plt.title("折线图示例")
-    plt.xlabel("索引")
-    plt.ylabel("值")
+    # 绘制折线图
+    # (line1_odd,) = plt.plot(
+    #     x_odd1,
+    #     list_retransmit,
+    #     label="var",
+    #     marker="o",
+    #     linestyle="-",
+    #     linewidth=1,
+    # )
+
+    # # 添加标题和标签
+    # plt.title("label")
+    # plt.xlabel("count")
+    # plt.ylabel("var")
+
+    # 添加鼠标悬停显示数值功能
+    # mplcursors.cursor([x_odd1, line1_odd], hover=True)
+
+    # 显示图形
+    # plt.show()
+
+    extruder_temp_list, bed_temp_list = stats.get_target_temp_list(list_dicts)
+    target_extruder_list = [t[0] for t in extruder_temp_list]
+    temp_extruder_list = [t[1] for t in extruder_temp_list]
+
+    LogKlipper.save_to_file(" ".join(temp_extruder_list), "out/temp_val.cfg")
+
+    target_bed_list = [t[0] for t in bed_temp_list]
+    temp_bed_list = [t[1] for t in bed_temp_list]
+
+    # 计算移动平均
+    data = pd.Series(temp_extruder_list)
+    print(data)
+
+    # 计算移动平均
+    rolling_mean = data.rolling(window=100).mean()  # 使用更大的窗口来平滑数据
+
+    # 绘制原始数据和移动平均线
+    plt.figure(figsize=(12, 6))  # 增加图表尺寸
+    # plt.plot(
+    #     data.index, data, label="Original Data", alpha=0.5, color="b"
+    # )  # 降低原始数据的透明度
+    plt.plot(
+        rolling_mean.index,
+        rolling_mean,
+        label="Moving Average",
+        linestyle="-",
+        color="r",
+    )
+
+    # 调整Y轴的范围和步长
+    plt.ylim(-200, 300)  # 根据数据调整Y轴范围
+    plt.yticks(np.arange(-200, 300, 10))  # 设置Y轴的步长为2
+
+    # 添加图例、标题和坐标轴标签
+    plt.legend()
+    plt.title("Original Data vs. Moving Average")
+    plt.xlabel("Index")
+    plt.ylabel("Value")
 
     # 显示图形
     plt.show()
+
+# print(temp_extruder_list)
+
+# 创建 x 轴数据
+# x_odd1 = range(len(target_extruder_list))
+# x_even1 = range(len(temp_extruder_list))
+# x_odd2 = range(len(target_bed_list))
+# x_even2 = range(len(temp_bed_list))
+# print(x_odd1, x_even1)
+
+# 绘制折线图
+# plt.scatter(
+#     x_odd1,
+#     target_extruder_list,
+#     label="target extruder",
+#     linewidth=0.3,
+#     color="green",
+#     alpha=0.7,
+# )
+
+# plt.plot(
+#     x_even1,
+#     temp_extruder_list,
+#     label="temp extruder",
+#     linewidth=1,
+#     alpha=0.7,
+# )
+
+# (line2_odd,) = plt.plot(
+#     x_odd2,
+#     target_bed_list,
+#     label="target bed",
+#     marker="o",
+#     linestyle="-",
+#     linewidth=1,
+# )
+# (line2_even,) = plt.plot(
+#     x_even2,
+#     temp_bed_list,
+#     label="temp bed",
+#     linewidth=0.3,
+#     alpha=0.7,
+# )
+
+# 添加标题和标签
+# plt.title("Odd and Even Values Line Plot for Two Lists")
+# plt.xlabel("Index")
+# plt.ylabel("Value")
+# # plt.ylim(-330, 330)
+# # plt.gca().invert_yaxis()
+# plt.legend()
+# plt.yticks([])
+# plt.xticks([])
+# 添加点击标签隐藏/显示线条的功能
+# def on_pick(event):
+#     line = event.artist
+#     line.set_visible(not line.get_visible())
+#     plt.draw()
+
+# 添加鼠标悬停显示数值功能
+# mplcursors.cursor([line1_odd, line1_even, line2_odd, line2_even], hover=True)
+
+# # 连接点击事件
+# plt.gcf().canvas.mpl_connect("pick_event", on_pick)
+
+# # 使线条可拾取
+# line1_odd.set_picker(True)
+# line1_even.set_picker(True)
+# line2_odd.set_picker(True)
+# line2_even.set_picker(True)
+
+# 显示图形
+# plt.grid(True)
+# plt.show()
--- paser_log/paser_log.py
+++ paser_log/paser_log.py
@@ -27,7 +27,32 @@ class LogErrorMsg:
         return "警告: 没有发现错误信息"
 
 
+class LogKlipper:
+    def __init__(self, log) -> None:
+        self.log = log
+
+    def extract_newest_config(self):
+        if self.log is not None and self.log != "":
+            content = self.log
+            end_index = content.rfind("=======================")
+            if end_index != -1:
+                start_index = content.rfind("===== Config file =====", 0, end_index)
+                middle_content = content[
+                    start_index + len("===== Config file =====") : end_index
+                ].strip()
+                return middle_content
+        return "警告: 未找到配置"  # todo，错误信息统一由类返回
+
+    @staticmethod
+    def save_to_file(cfg, save_path="out/klipper.cfg"):
+        with open(save_path, "w") as file:
+            file.write(cfg)
+
+
 class LogTemp:
+    def __init__(self, log) -> None:
+        self.log = log
+
     def __get_temp_info(self, target_str):
         start_index = self.log.rfind(target_str) + len(target_str)
         if start_index != -1:
@@ -46,27 +71,6 @@ class LogTemp:
         return temp_dict
 
 
-class LogKlipper:
-    def __init__(self, log) -> None:
-        self.log = log
-
-    def extract_newest_config(self):
-        if self.log is not None and self.log != "":
-            content = self.log
-            end_index = content.rfind("=======================")
-            if end_index != -1:
-                start_index = content.rfind("===== Config file =====", 0, end_index)
-                middle_content = content[
-                    start_index + len("===== Config file =====") : end_index
-                ].strip()
-                return middle_content
-        return "警告: 未找到配置"  # todo，错误信息统一由类返回
-
-    def save_to_file(self, cfg, save_path="out\klipper.cfg"):
-        with open(save_path, "w") as file:
-            file.write(cfg)
-
-
 class LogStats:
     def __init__(self, file_path) -> None:
         self.file_path = file_path
@@ -81,21 +85,31 @@ class LogStats:
         except FileNotFoundError:
             return "错误: 文件未找到"
 
-    def __parse_stats_info(self, stats_string):
-        # Use regular expressions to find all key-value pairs, including stats values
-        pattern = r"(Stats)\s([\d\.]+)|(\S+)=([\d\.]+)"
-        matches = re.findall(pattern, stats_string)
+    def __parse_stats_key_info(self, stats_string):
+        out_dicts = {}
 
-        # Store matching results in a dictionary, one list for each key
-        stats_dict = defaultdict(list)
-        for match in matches:
-            if match[0] == "Stats":
-                stats_dict["Stats"].append(float(match[1]))
-            else:
-                key = match[2]
-                value = float(match[3])
-                stats_dict[key].append(value)
-        return stats_dict
+        heater_bed_match = re.search(
+            r"heater_bed:\s*target=(\d+)\s*temp=([\d.]+)", stats_string
+        )
+        if heater_bed_match:
+            target = heater_bed_match.group(1)
+            temp = heater_bed_match.group(2)
+            out_dicts["heater_bed"] = {"target": target, "temp": temp}
+
+        extruder_match = re.search(
+            r"extruder:\s*target=(\d+)\s*temp=([\d.]+)", stats_string
+        )
+        if extruder_match:
+            target = extruder_match.group(1)
+            temp = extruder_match.group(2)
+            out_dicts["extruder"] = {"target": target, "temp": temp}
+
+        bytes_retransmit_matches = re.findall(r"bytes_retransmit=(\d+)", stats_string)
+        if bytes_retransmit_matches:
+            bytes_retransmit_list = [int(value) for value in bytes_retransmit_matches]
+            out_dicts["bytes_retransmit"] = bytes_retransmit_list
+
+        return out_dicts
 
     def get_stats_info(self):
         self.__generate_stats_list()
@@ -106,9 +120,63 @@ class LogStats:
 
         list_dict = []
         for stats_line in self.stats_list:
-            list_dict.append(self.__parse_stats_info(stats_line))
+            list_dict.append(self.__parse_stats_key_info(stats_line))
         return list_dict
 
-    def save_to_file(self, info, save_path="out\stats.cfg"):
+    def save_to_file(self, info, save_path="out/stats.cfg"):
         with open(save_path, "w") as file:
             file.write(info)
+
+    def get_bytes_retransmit_incremental_list(self, interval, list_dicts):
+        try:
+            i = 0
+            min_val = max_val = 0
+            list_retransmit = []
+            min_last_val = max_last_val = 0
+
+            for dicts in list_dicts:
+                min_last_val = min(
+                    dicts["bytes_retransmit"]
+                )  # Here min_last_val and max_last_val are generally the same value
+                max_last_val = max(dicts["bytes_retransmit"])
+
+                if min_last_val < min_val:
+                    min_val = min_last_val
+
+                if max_last_val > max_val:
+                    max_val = max_last_val
+
+                i += 1
+                if interval == i:
+                    i = 0
+                    list_retransmit.append(max_val - min_val)
+                    min_val = min_last_val  # Starting from the last result
+                    max_val = max_last_val
+
+            if len(dicts) % interval != 0:
+                list_retransmit.append(max_val - min_val)
+
+            # print(list_retransmit, len(list_retransmit))
+            return list_retransmit
+
+        except Exception as e:
+            print("异常：", e)
+
+    def get_target_temp_list(self, list_dicts):
+        try:
+            extruder_temp_list = []
+            bed_temp_list = []
+
+            for dicts in list_dicts:
+                bed_temp_list.append(
+                    (dicts["heater_bed"]["target"], dicts["heater_bed"]["temp"])
+                )
+                extruder_temp_list.append(
+                    (dicts["extruder"]["target"], dicts["extruder"]["temp"])
+                )
+
+            # print(extruder_temp_list)
+            return extruder_temp_list, bed_temp_list
+
+        except Exception as e:
+            print("异常：", e)
